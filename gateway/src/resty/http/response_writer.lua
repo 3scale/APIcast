d71c7f3c510059d553c55ac268a8fd1b8ec10024
An Tran <tkan145@gmail.com>
2026-03-03 14:44:04 +1000

    perf(response_writer): minimize socket:send() calls

--- gateway/src/resty/http/response_writer.lua
+++ gateway/src/resty/http/response_writer.lua
@@ -1,5 +1,7 @@
 local fmt = string.format
 local str_lower = string.lower
+local insert = table.insert
+local concat = table.concat
 
 local _M = {
 }
@@ -32,7 +34,6 @@ end
 -- write_response writes response body reader to sock in the HTTP/1.x server response format,
 -- The connection is closed if send() fails or when returning a non-zero
 function _M.send_response(sock, response, chunksize)
-    local bytes, err
     chunksize = chunksize or 65536
 
     if not response then
@@ -44,33 +45,32 @@ function _M.send_response(sock, response, chunksize)
         return nil, "socket not initialized yet"
     end
 
-    -- Status line
-    -- TODO: get HTTP version from request
-    local status = fmt("HTTP/%d.%d %03d %s\r\n", 1, 1, response.status, response.reason)
-    bytes, err = send(sock, status)
-    if not bytes then
-        return nil, "failed to send status line, err: " .. (err or "unknown")
-    end
+    -- Build status line + headers into a single buffer to minimize send() calls
+    local buf = {
+        fmt("HTTP/1.1 %03d %s\r\n", response.status, response.reason)
+    }
 
      -- Filter out hop-by-hop headeres
      for k, v in pairs(response.headers) do
         if not HOP_BY_HOP_HEADERS[str_lower(k)] then
-            local header = fmt("%s: %s\r\n", k, v)
-            bytes, err = sock:send(header)
-            if not bytes then
-              return nil, "failed to send status line, err: " .. (err or "unknown")
-            end
+          insert(buf, k .. ": " .. v .. cr_lf)
         end
      end
 
     -- End-of-header
-    bytes, err = send(sock, cr_lf)
+    insert(buf, cr_lf)
+
+    local bytes, err = sock:send(concat(buf))
     if not bytes then
-        return nil, "failed to send status line, err: " .. (err or "unknown")
+        return nil, "failed to send headers, err: " .. (err or "unknown")
     end
 
     -- Write body
     local reader = response.body_reader
+    if not reader then
+      return nil, "no body reader"
+    end
+
     repeat
         local chunk, read_err
 
